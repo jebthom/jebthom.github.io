75b2edb5d86cbedcba4ad4013b87c797e9022740
jebthom <65605958+jebthom@users.noreply.github.com>
2025-10-09 17:28:33 +0000

    Deploying to gh-pages from @ jebthom/jebthom.github.io@8065e4cc139268801116e2c3cd8c1cdb72063451 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "This page is under construction.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This page is under construction! I am not Albert Einstein.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
